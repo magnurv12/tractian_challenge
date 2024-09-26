994f4e48c86df12606500fe3c0f41e4b33ccaf2e
Magnussen Dourado <magnurv@gmail.com>
2024-09-26 19:41:56 -0300

    fix: tree builder

--- apps/base_app/lib/src/presentation/views/assets/assets_page.dart
+++ apps/base_app/lib/src/presentation/views/assets/assets_page.dart
@@ -3,18 +3,16 @@ import 'package:dependencies/dependencies.dart';
 import 'package:flutter/material.dart';
 
 import '../../../domain/domain.dart';
-import '../views.dart';
+import '../shared/shared.dart';
+import 'assets_state.dart';
+import 'assets_viewmodel.dart';
 
 /// A page that displays the details of an asset.
 ///
 /// The [AssetsPage] requires an [id] to identify which asset to display.
 class AssetsPage extends StatefulWidget {
-  ///
-  /// Parameters:
-  /// - `id`: The unique identifier of the asset to be displayed.
   final String id;
 
-  /// Constructor [AssetsPage]
   const AssetsPage({
     super.key,
     required this.id,
@@ -42,20 +40,16 @@ class _AssetsPageState extends ViewState<AssetsPage, AssetsViewmodel> {
                 backgroundColor: Theme.of(context).colorScheme.inversePrimary,
                 title: const Text('Assets Page'),
               ),
-              body: CustomScrollView(
-                slivers: [
-                  SliverList(
-                    delegate: SliverChildBuilderDelegate(
-                      (context, index) {
-                        final node = nodes[index];
-                        return _ExpansionTitle(
-                          node: node,
-                        );
-                      },
-                      childCount: nodes.length,
-                    ),
-                  ),
-                ],
+              body: ListView.builder(
+                shrinkWrap: true,
+                physics: const ClampingScrollPhysics(),
+                itemBuilder: (context, index) {
+                  final node = nodes[index];
+                  return _ExpansionTitle(
+                    node: node,
+                  );
+                },
+                itemCount: nodes.length,
               ),
             ),
           AssetsStateError() =>
@@ -75,8 +69,8 @@ class _AssetsPageState extends ViewState<AssetsPage, AssetsViewmodel> {
 
 class _ExpansionTitle extends StatefulWidget {
   final TreeNode node;
+
   const _ExpansionTitle({
-    super.key,
     required this.node,
   });
 
@@ -85,11 +79,49 @@ class _ExpansionTitle extends StatefulWidget {
 }
 
 class _ExpansionTitleState extends State<_ExpansionTitle> {
-  bool _isExpanded = true;
+  bool _isExpanded = false;
+  bool _isVisible = false;
+  final GlobalKey _key = GlobalKey();
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) => _checkVisibility());
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(covariant _ExpansionTitle oldWidget) {
+    if (oldWidget != widget) {
+      _checkVisibility();
+      super.didUpdateWidget(oldWidget);
+    }
+  }
+
+  void _checkVisibility() {
+    final renderObject = _key.currentContext?.findRenderObject();
+    if (renderObject is RenderBox) {
+      final offset = renderObject.localToGlobal(Offset.zero); // Posição na tela
+      final screenHeight = MediaQuery.of(context).size.height;
+
+      final isVisible =
+          offset.dy < screenHeight && offset.dy + renderObject.size.height > 0;
+
+      if (_isVisible != isVisible) {
+        setState(() {
+          _isVisible = isVisible;
+          _isExpanded = _isVisible;
+        });
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    // Recalcula a visibilidade após o frame ser renderizado
+    WidgetsBinding.instance.addPostFrameCallback((_) => _checkVisibility());
+
     return Column(
+      key: _key,
       children: [
         ListTile(
           title: Text(widget.node.name),
@@ -103,30 +135,31 @@ class _ExpansionTitleState extends State<_ExpansionTitle> {
             ? Padding(
                 padding: const EdgeInsets.only(left: 16.0),
                 child: switch (widget.node) {
-                  Location(:final children) => SliverList(
-                      delegate: SliverChildBuilderDelegate(
-                        (context, index) {
-                          final node = children[index];
-                          return _ExpansionTitle(
-                            node: node,
-                          );
-                        },
-                        childCount: children.length,
-                      ),
+                  Location(:final children) => ListView.builder(
+                      physics: const ClampingScrollPhysics(),
+                      itemCount: children.length,
+                      shrinkWrap: true,
+                      itemBuilder: (context, index) {
+                        final node = children[index];
+                        return _ExpansionTitle(
+                          node: node,
+                        );
+                      },
                     ),
-                  Asset(:final children) => SliverList(
-                      delegate: SliverChildBuilderDelegate(
-                        (context, index) {
-                          final node = children[index];
-                          return _ExpansionTitle(
-                            node: node,
-                          );
-                        },
-                        childCount: children.length,
-                      ),
+                  Asset(:final children) => ListView.builder(
+                      physics: const ClampingScrollPhysics(),
+                      itemCount: children.length,
+                      shrinkWrap: true,
+                      itemBuilder: (context, index) {
+                        final node = children[index];
+                        return _ExpansionTitle(
+                          node: node,
+                        );
+                      },
                     ),
                   _ => const SizedBox.shrink(),
                 },
+                // Se não for visível, não renderiza os filhos
               )
             : const SizedBox.shrink(),
       ],
